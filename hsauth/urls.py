2fcd058e71580c80e46484eeaf09b14d721a4ec6
CVi <christoffer@viken.me>
2015-02-23 15:09:01 +0100

    Fixed login engine, added updates profile on login

--- hsauth/urls.py
+++ hsauth/urls.py
@@ -7,6 +7,6 @@ from django.contrib.auth.decorators import login_required
 
 
 urlpatterns = patterns('',
-                       url(r'^login/(?P<provider>(\w|-)+)/$', login_required(HSAuthRedirect.as_view()), name='associate'),
-                       url(r'^callback/(?P<provider>(\w|-)+)/$', login_required(HSCallback.as_view()), name='associate-callback'),
+                       url(r'^login/(?P<provider>(\w|-)+)/$', HSAuthRedirect.as_view(), name='associate'),
+                       url(r'^callback/(?P<provider>(\w|-)+)/$', HSCallback.as_view(), name='associate-callback'),
                        )
--- hsauth/views.py
+++ hsauth/views.py
@@ -1,4 +1,5 @@
 from allaccess.views import OAuthCallback
+from django.shortcuts import redirect
 from django.views.generic import View
 from django.contrib.auth import authenticate, login, get_user_model
 import logging
@@ -13,6 +14,7 @@ logger = logging.getLogger('hsauth.views')
 
 class HSAuthRedirect(OAuthRedirect):
     def get_callback_url(self, provider):
+        print(reverse('associate-callback', kwargs={'provider': provider.name}))
         return reverse('associate-callback', kwargs={'provider': provider.name})
 
 
@@ -29,3 +31,21 @@ class HSCallback(OAuthCallback, View):
             'last_name': info.get('last_name')
         }
         return User.objects.create_user(**kwargs)
+
+    def handle_existing_user(self, provider, user, access, info):
+        "Login user and redirect."
+        login(self.request, user)
+        s = False
+        print("Hello")
+        if user.email != info.get('email'):
+            user.email = info.get('email')
+            s = True
+        if user.first_name != info.get('first_name'):
+            user.first_name = info.get('first_name')
+            s = True
+        if user.last_name != info.get('last_name'):
+            user.last_name = info.get('last_name')
+            s = True
+        if s:
+            user.save()
+        return redirect(self.get_login_redirect(provider, user, access))
